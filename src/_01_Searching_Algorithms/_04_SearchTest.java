ae5ae67d18e743bb7434a53ee3102d93c2e6ff27
League <League@DESKTOP-A86R9LN>
2020-08-12 15:30:02 -0700

    A bunch of algorithms

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -8,6 +8,9 @@ class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
 	_00_LinearSearch linearSearch = new _00_LinearSearch();
+	_01_BinarySearch binarySearch = new _01_BinarySearch();
+	_02_InterpolationSearch interpolationSearch = new _02_InterpolationSearch();
+	_03_ExponentialSearch exponentialSearch = new _03_ExponentialSearch();
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
@@ -21,17 +24,33 @@ class _04_SearchTest {
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] testArray = new int[] {0,1,2,3,4,5,6,7,8,9,10};
+		assertEquals(binarySearch.binarySearch(testArray, 0, testArray.length, 0), 0);
+		assertEquals(binarySearch.binarySearch(testArray, 0, testArray.length, 1), 1);
+		assertEquals(binarySearch.binarySearch(testArray, 0, testArray.length, 2), 2);
+		assertEquals(binarySearch.binarySearch(testArray, 0, testArray.length, 3), 3);
+		assertEquals(binarySearch.binarySearch(testArray, 0, testArray.length, 4), 4);
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[] testArray = new int[] {0,1,2,3,4,5,6,7,8,9,10};
+		assertEquals(interpolationSearch.interpolationSearch(testArray, 0), 0);
+		assertEquals(interpolationSearch.interpolationSearch(testArray, 5), 5);
+		assertEquals(interpolationSearch.interpolationSearch(testArray, 10), 10);
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		int[] testArray = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		assertEquals(exponentialSearch.exponentialSearch(testArray, 0), 0);
+		assertEquals(exponentialSearch.exponentialSearch(testArray, 20), 20);
+		assertEquals(exponentialSearch.exponentialSearch(testArray, 5), 5);
+		assertEquals(exponentialSearch.exponentialSearch(testArray, 7), 7);
+		assertEquals(exponentialSearch.exponentialSearch(testArray, 9), 9);
 	}
 }
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -3,7 +3,131 @@ package _02_More_Algorithms;
 import java.util.List;
 
 public class Algorithms {
+
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i) == "cracked") {
+				return i;
+			}
+		}
+		return -1;
 	}
+
+	public static int countPearls(List<Boolean> list) {
+		int pearls = 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == true) {
+				pearls++;
+			}
+		}
+		return pearls;
+	}
+
+	public static double findTallest(List<Double> list) {
+		double largest = 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) > largest) {
+				largest = list.get(i);
+			}
+		}
+		return largest;
+	}
+
+	public static String findLongestWord(List<String> list) {
+		String longest = "";
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).length() > longest.length()) {
+				longest = list.get(i);
+			}
+		}
+		return longest;
+	}
+
+	public static boolean containsSOS(List<String> list) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).contains("... --- ...")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static List<Double> sortScores(List<Double> original) {
+		List<Double> list = original;
+		while (!doubleListSorted(list)) {
+			for (int i = 0; i < list.size() - 1; i++) {
+				if (list.get(i) > list.get(i + 1)) {
+					double temp = list.get(i);
+					list.set(i, list.get(i + 1));
+					list.set(i + 1, temp);
+				}
+			}
+		}
+		return list;
+	}
+
+	static boolean doubleListSorted(List<Double> list) {
+		for (int i = 0; i < list.size() - 1; i++) {
+			if (list.get(i) > list.get(i + 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static List<String> sortDNA(List<String> DNA) {
+		List<String> sortedList = DNA;
+		boolean isSorted = false;
+		
+		while (!isSorted) {
+			
+			for(int i = 0; i < sortedList.size() - 1; i++) {
+				if(sortedList.get(i).length() > sortedList.get(i + 1).length()) {
+					String temp = sortedList.get(i);
+					sortedList.set(i, sortedList.get(i + 1));
+					sortedList.set(i + 1, temp);
+				}
+			}
+			
+			for (int i = 0; i < sortedList.size() - 1; i++) {
+				if (sortedList.get(i).length() > sortedList.get(i + 1).length()) {
+					isSorted = false;
+					i = sortedList.size();
+				} else {
+					isSorted = true;
+				}
+			}
+		}
+		return sortedList;
+
+	}
+
+	public static List<String> sortWords(List<String> list){
+	
+		List<String> sortedList = list;
+		boolean isSorted = false;
+		
+		while (!isSorted) {
+			
+			for(int i = 0; i < sortedList.size() - 1; i++) {
+				if(sortedList.get(i).compareTo(sortedList.get(i+1)) > 0) {
+					String temp = sortedList.get(i);
+					sortedList.set(i, sortedList.get(i + 1));
+					sortedList.set(i + 1, temp);
+				}
+			}
+			
+			for (int i = 0; i < sortedList.size() - 1; i++) {
+				if (sortedList.get(i).compareTo(sortedList.get(i+1)) > 0) {
+					isSorted = false;
+					i = sortedList.size();
+				} else {
+					isSorted = true;
+				}
+			}
+		}
+		return sortedList;
+		
+	}
+	
 }
